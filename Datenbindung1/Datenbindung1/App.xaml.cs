d7e42b361a82fc8ca5384faa7accee72d2d747c1
drjofu <drjofu@web.de>
2023-05-11 16:34:22 +0200

    Tag 1

---
+++ Datenbindung1/Datenbindung1/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Markup;
+
+namespace Datenbindung1
+{
+  /// <summary>
+  /// Interaction logic for App.xaml
+  /// </summary>
+  public partial class App : Application
+  {
+    static App()
+    {
+      string text = string.Format("{0:0.0}", 1.2345);
+
+      FrameworkElement.LanguageProperty.OverrideMetadata(
+        typeof(FrameworkElement),
+        new FrameworkPropertyMetadata(
+          XmlLanguage.GetLanguage(
+             CultureInfo.CurrentCulture.Name)
+          )
+        );
+
+    }
+  }
+}
