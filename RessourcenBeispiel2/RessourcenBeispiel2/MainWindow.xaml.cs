c7e9a5766507ab56704b0db7eaff6ed2a37e5913
drjofu <drjofu@web.de>
2023-05-11 15:33:27 +0200

    Tag 1

---
+++ RessourcenBeispiel2/RessourcenBeispiel2/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace RessourcenBeispiel2
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    private void Ändern1(object sender, RoutedEventArgs e)
+    {
+      var brush = (LinearGradientBrush) this.Resources["LGB"];
+      brush.GradientStops[0].Color = Colors.Red;
+    }
+
+    private void Ändern2(object sender, RoutedEventArgs e)
+    {
+      Resources["LGB"] = new LinearGradientBrush(Colors.Pink, Colors.Orange, 30);
+    }
+  }
+}
