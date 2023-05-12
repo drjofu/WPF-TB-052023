7ff1fd424ac5896f3f67db6092325c9839fc4887
drjofu <drjofu@web.de>
2023-05-12 10:30:23 +0200

    Tag 2

---
+++ Datenbindung2/Datenbindung2/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace Datenbindung2
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+      this.DataContext = new Firma();
+    }
+  }
+}
---
+++ Datenbindung2/Datenbindung2/Person.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace Datenbindung2
+{
+  class Person
+  {
+    public string Vorname { get; set; }
+    public string Nachname { get; set; }
+    public int Alter { get; set; }
+
+    public bool IstErfahren => Alter > 65;
+  }
+
+  class Firma
+  {
+    public string Name { get; set; } = "Hinz & Kunz";
+    public List<Person> Mitarbeiter { get; set; } = new()
+    {
+        new Person() {Vorname="Dagobert", Nachname="Duck", Alter=78},
+        new Person() {Vorname="Donald", Nachname="Duck", Alter=55},
+        new Person() {Vorname="Micky", Nachname="Mouse", Alter=67},
+        new Person() {Vorname="Daisy", Nachname="Duck", Alter=45},
+        new Person() {Vorname="Panzer", Nachname="Knacker", Alter=34},
+        new Person() {Vorname="Gustav", Nachname="Gans", Alter=25}
+    };
+  }
+}
