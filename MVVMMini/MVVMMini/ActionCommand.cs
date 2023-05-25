46e9254e4a76dc31406e830db1d787c26e041cb4
drjofu <drjofu@web.de>
2023-05-25 12:12:19 +0200

    Tag 3

---
+++ MVVMMini/MVVMMini/ActionCommand.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace MVVMMini
+{
+  public class ActionCommand : ICommand
+  {
+    private readonly Action action;
+    private readonly Action<object> action2;
+
+    public ActionCommand(Action action)
+    {
+      this.action = action;
+    }
+
+    public ActionCommand(Action<object> action)
+    {
+      action2 = action;
+    }
+
+    private bool isEnabled = true;
+
+    public bool IsEnabled
+    {
+      get { return isEnabled; }
+      set
+      {
+        isEnabled = value;
+        CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+      }
+    }
+
+
+    public event EventHandler? CanExecuteChanged;
+
+    public bool CanExecute(object? parameter)
+    {
+      return isEnabled;
+    }
+
+    public void Execute(object? parameter)
+    {
+      action?.Invoke();
+      action2?.Invoke(parameter);
+    }
+  }
+}
---
+++ MVVMMini/MVVMMini/ViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMMini
+{
+  public class ViewModel : INotifyPropertyChanged
+  {
+    public event PropertyChangedEventHandler PropertyChanged;
+    protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    private int zahl1;
+
+    public int Zahl1
+    {
+      get { return zahl1; }
+      set { zahl1 = value; }
+    }
+
+    private int zahl2;
+
+    public int Zahl2
+    {
+      get { return zahl2; }
+      set { zahl2 = value; }
+    }
+
+    private int ergebnis;
+
+    public int Ergebnis
+    {
+      get { return ergebnis; }
+      set { ergebnis = value; OnPropertyChanged(); }
+    }
+
+    public ActionCommand PlusCommand { get; set; }
+    public ActionCommand MinusCommand { get; set; }
+
+
+    private void Plus()
+    {
+      // hier würde das Model aufgerufen werden
+
+    }
+  }
+}
